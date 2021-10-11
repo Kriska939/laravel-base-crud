cd42b72e593a55a21b5f6bf310dde87f258c6910
Kriska939 <cristinacorrado93@gmail.com>
2021-10-11 23:44:52 +0200

    style fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,8 @@
 
 @section('content')
 
-PRINT IN home
+<div class="card-title text-center">
+    <img src="{{asset('images/dc-logo-bg.png')}}" width="300" alt="dc background" class="img-fluid">
+    <h1 class="mt-5 text-primary fancy">Welcome to the DC Library</h1>
+</div>
 @endsection
